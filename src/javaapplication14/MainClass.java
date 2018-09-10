e0213dd6d2334bf7d2873d2c35b13fdca2ad1257
batyukvn <sibnskbvn91@yandex.ru>
2018-09-10 16:16:15 +0700

    Все как есть в пн

--- src/javaapplication14/MainClass.java
+++ src/javaapplication14/MainClass.java
@@ -1,12 +1,22 @@
 package javaapplication14;
 
+import java.awt.Container;
+import java.awt.Graphics;
+
 public class MainClass {
 
  
     public static void main(String[] args) {
         Frame start = new Frame();
         start.setLocation(700, 300);
-        start.setVisible(true);
+       // start.setVisible(true);
+        
+        Snake snake = new Snake();
+        
+        snake.setLocation(700, 300);
+        snake.setVisible(true);
+        
+  
     }
     
 }
